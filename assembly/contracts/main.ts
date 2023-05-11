c1ecc5ed365858d1c126c63eda8dcaf2bfeba55f
fengmao <onebitbank@gmail.com>
2023-05-11 17:11:49 +0800

    v0.0.1

--- assembly/contracts/main.ts
+++ assembly/contracts/main.ts
@@ -1,5 +1,13 @@
 import { Storage, Context, generateEvent } from '@massalabs/massa-as-sdk';
 import { Args } from '@massalabs/as-types';
+import { JSON } from "json-as/assembly";
+
+@json
+class Tweet {
+  content!: string;
+  author!: string;
+  cdate!: i64;
+}
 
 /**
  * This function is meant to be called only one time: when the contract is deployed.
@@ -12,43 +20,51 @@ export function constructor(_: StaticArray<u8>): void {
   if (!Context.isDeployingContract()) {
     return;
   }
-  const nBlogPosts = 0;
-  // Set the initial value of N_BLOG_POSTS to 0 in the storage of the contract
-  Storage.set("N_BLOG_POSTS", nBlogPosts.toString());
+  const nTweets = 0;
+  // Set the initial value of N_TWEETS to 0 in the storage of the contract
+  Storage.set("N_TWEETS", nTweets.toString());
   // This function is used to emit an event to the blockchain
-  generateEvent('Blog initiated');
+  generateEvent('MassaPad initiated');
 }
 
-export function post(_args: StaticArray<u8>): void {
+export function tweet(_args: StaticArray<u8>): void {
   const args = new Args(_args);
-  // Get the post from the arguments and check if it is valid (not null)
-  const post = args
+  // Get the tweet from the arguments and check if it is valid (not null)
+  const content = args
     .nextString()
-    .expect('Post argument is missing or invalid');
+    .expect('Content argument is missing or invalid');
 
-  let postLastIndex = 0;
-  if (Storage.get<string>("N_BLOG_POSTS") !== "") {
-    postLastIndex = parseInt(Storage.get("N_BLOG_POSTS")) as i32;
+  const author = args
+    .nextString()
+    .expect('Author argument is missing or invalid');
+
+  // tweet create date
+  const cdate = Date.now();
+
+  const tweet: Tweet = {
+    content: content,
+    author: author,
+    cdate: cdate,
+  };
+
+  let tweetLastIndex = 0;
+  if (Storage.get<string>("N_TWEETS") !== "") {
+    tweetLastIndex = parseInt(Storage.get("N_TWEETS")) as i32;
   }
-  postLastIndex += 1;
-  // Store the post in the storage of the contract with the key POST_postIndex
-  // The keys will have the following syntaxes: POST_1, POST_2, POST_3, etc.
-  Storage.set(blogKey(postLastIndex.toString()), post);
-  // Incrementing the value of N_BLOG_POSTS in the storage of the contract
-  Storage.set("N_BLOG_POSTS", postLastIndex.toString());
+  tweetLastIndex += 1;
+  // Store the tweet in the storage of the contract with the key TWEET_postIndex
+  // The keys will have the following syntaxes: TWEET_1, TWEET_2, TWEET_3, etc.
+  Storage.set(`TWEET_${tweetLastIndex}`, JSON.stringify<Tweet>(tweet));
+  // Incrementing the value of N_TWEETS in the storage of the contract
+  Storage.set("N_TWEETS", tweetLastIndex.toString());
 }
 
-export function deletePost(_args: StaticArray<u8>): void {
+export function deleteTweet(_args: StaticArray<u8>): void {
   const args = new Args(_args);
-  // Get the post index from the arguments and check if it is valid, we expect a string of the form "1.0" or "2.0", etc.
-  const postIndex = args
+  // Get the tweet index from the arguments and check if it is valid, we expect a string of the form "1.0" or "2.0", etc.
+  const tweetIndex = args
     .nextString()
-    .expect('Post index argument is missing or invalid');
-    // Delete the post from the storage of the contract by setting its value to an empty string
-  Storage.set(blogKey(postIndex), "");
-}
-
-// This function is used within the contract to generate the key of a post that will be stored in the storage
-export function blogKey(postIndex: string): string {
-  return "POST_" + postIndex;
+    .expect('Tweet index argument is missing or invalid');
+    // Delete the tweet from the storage of the contract by setting its value to an empty string
+  Storage.set(`TWEET_${tweetIndex}`, "");
 }
--- src/deploy.ts
+++ src/deploy.ts
@@ -22,6 +22,8 @@ const __filename = fileURLToPath(import.meta.url);
 
 const __dirname = path.dirname(path.dirname(__filename));
 
+console.log(__dirname);
+
 (async () => {
   await deploySC(
     publicApi,
